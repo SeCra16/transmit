780956779e05415f62aee38a93a606d972f8fb88
Sunny Golovine <sunny@golovinemail.org>
2016-11-25 13:15:22 -0500

    updated dbsetup.sql

--- db/dbsetup.sql
+++ db/dbsetup.sql
@@ -1,55 +1,44 @@
-CREATE TABLE fileshare_system.timestamp (
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  timestamp DATETIME NOT NULL,
-  hour INT NOT NULL,
-  minute INT NOT NULL,
-  second INT NOT NULL,
-  month INT NOT NULL,
-  day INT NOT NULL,
-  year INT NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX `timestamp_UNIQUE` (id ASC));
+CREATE TABLE `expiration_date` (
+  `id` int(11) NOT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  KEY `expiration_id_idx` (`id`),
+  CONSTRAINT `expiration_id` FOREIGN KEY (`id`) REFERENCES `timestamp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE fileshare_system.true_directory (
-  real_path VARCHAR(45) NOT NULL,
-  id INT NOT NULL,
-  UNIQUE INDEX realpath_UNIQUE (real_path ASC),
-  INDEX true_directory_id_idx (id, ASC),
-  CONSTRAINT true_directory_id
-    FOREIGN KEY (id)
-    REFERENCES fileshare_system.timestamp(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
+CREATE TABLE `false_directory` (
+CREATE TABLE `false_directory` (
+  `id` int(11) NOT NULL,
+  `fake_path` varchar(45) NOT NULL,
+  UNIQUE KEY `fake_path_UNIQUE` (`fake_path`),
+  KEY `upload_id_idx` (`id`),
+  CONSTRAINT `upload_id_fake` FOREIGN KEY (`id`) REFERENCES `timestamp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE fileshare_system.false_directory (
-  id INT NOT NULL,
-  fake_path VARCHAR(10) NOT NULL,
-  INDEX `false_directory_id_idx` (id ASC),
-  CONSTRAINT false_directory_id_fake
-    FOREIGN KEY (id)
-    REFERENCES fileshare_system.timestamp (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-);
+CREATE TABLE `file_metadata` (
+  `id` int(11) NOT NULL,
+  `file_type` varchar(10) DEFAULT NULL,
+  `file_size` int(255) DEFAULT NULL,
+  KEY `id_descriptor_idx` (`id`),
+  CONSTRAINT `id_descriptor` FOREIGN KEY (`id`) REFERENCES `timestamp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE fileshare_system.file_metadata (
-  id INT NOT NULL,
-  file_type VARCHAR(10) NOT NULL,
-  file_size INT NOT NULL,
-  INDEX file_metadata_id_idx (id ASC),
-  CONSTRAINT file_metadata_id
-    FOREIGN KEY (id)
-    REFERENCES fileshare_system.timestamp (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE );
+CREATE TABLE `timestamp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `hour` int(11) DEFAULT NULL,
+  `minute` int(11) DEFAULT NULL,
+  `month` int(11) DEFAULT NULL,
+  `day` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `second` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uploadnumber_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-CREATE TABLE fileshare_system.expiration_date (
-  id INT NOT NULL,
-  expiration_date DATETIME NOT NULL,
-  INDEX expiration_date_idx (id ASC),
-  CONSTRAINT expiration_date_id
-    FOREIGN KEY (id)
-    REFERENCES fileshare_system.timestamp (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
-
+CREATE TABLE `true_directory` (
+  `real_path` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL,
+  UNIQUE KEY `real_path_UNIQUE` (`real_path`),
+  KEY `upload_id_idx` (`id`),
+  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `timestamp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
